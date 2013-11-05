88581a9eacc449133bb240c0c1bb8a7b2e66623f
Indraniel <indraniel@gmail.com>
2013-11-04 21:09:31 -0600

    + convert to using Pillow/PIL

--- mosaic.py
+++ mosaic.py
@@ -1,124 +1,147 @@
 #!/usr/bin/env python
 
 import argparse
-import kaa.imlib2 as im2
 import math
 import random
 import os
 import sys
-import signal
-import struct
+
+from PIL import Image
 
 class PixelArt(object):
     def __init__(self, grid_width=7, grid_height=7, min_opacity=20, max_opacity=70):
-        self.grid_width = grid_width
+        self.grid_width  = grid_width
         self.grid_height = grid_height
         self.min_opacity = min_opacity
         self.max_opacity = max_opacity
 
     def gridify(self, imgfile):
-        img = im2.open(imgfile)
-        print "%-10s : %s" % ("filename", img.filename)
-        print "%-10s : %s" % ("format", img.format)
-        print "%-10s : %s" % ("width", img.width)
-        print "%-10s : %s" % ("height", img.height)
-        alpha_weights = self.generate_alpha_weights(img)
-        print "len(weights) = %d" % len(alpha_weights)
-        print "len(weights[0]) = %d" % len(alpha_weights[0])
-        buf = img.get_raw_data(write=False)
-        print "len(buf) = %d" % len(buf)
-        img2 = im2.new((img.width, img.height), bytes='\x00\x00\x00\x00' * img.width * img.height)
-        buf2 = img2.get_raw_data(write=True)
+        img = Image.open(imgfile)
 
-        for x in range(img2.height):
-            for y in range(img2.width):
-                (b, g, r, a) = self.get_pixel_index(x, y, img2.width, img2.height)
+        # display some basic information
+        (width, height) = img.size
+        print "%-10s : %s" % ("filename", imgfile)
+        print "%-10s : %s" % ("format", img.format)
+        print "%-10s : %s" % ("mode", img.mode)
+        print "%-10s : %s" % ("width", width)
+        print "%-10s : %s" % ("height", height)
+        print
+
+        if img.mode != 'RGBA':
+            img = img.convert('RGBA')
+
+        alpha_weights = self.generate_alpha_weights(img)
+        self.tint(img, alpha_weights)
+        grid_img_file = self.save_image(img, imgfile)
+        return grid_img_file
+
+    def tint(self, img, alpha_weights):
+        (width, height) = img.size
+        pixels = img.load()
+
+        for x in range(width):
+            for y in range(height):
+                (b, g, r, a) = pixels[x, y]
 #                print '[x:%d, y:%d] - b:%d g:%d r:%d a:%d' % (x, y, b, g, r, a)
 
                 # On a "grid" border, draw the black grid line
-                if (x % self.grid_height == 0 or y % self.grid_width == 0):
-                    buf2[b] = chr(0)
-                    buf2[g] = chr(0)
-                    buf2[r] = chr(0)
-                    buf2[a] = chr(100)
+                if (x % self.grid_width == 0 or y % self.grid_height == 0):
+                    pixels[x, y] = (0, 0, 0, 255)
 
                 # Otherwise, we're within a "grid", so tint it
                 else:
-                    grid_x = x/self.grid_height
-                    grid_y = y/self.grid_width
+                    grid_x = x/self.grid_width
+                    grid_y = y/self.grid_height
                     try:
-                        opts = alpha_weights[grid_x][grid_y]
+                        opacity = alpha_weights[grid_x][grid_y]
                     except IndexError:
                         print 'IndexError:'
                         print '\tgrid_x: %d' % grid_x
                         print '\tgrid_y: %d' % grid_y
                         sys.exit(1)
 
-                    alpha  = opts['alpha'] / 255.0
-                    color  = opts['color']
+                    for color in ('black', 'white'):
+                        pixels[x,y] = \
+                            self.alpha_composite(pixels[x,y], opacity, color)
 
-#                    fmt = 'x: %d y: %d grid_x: %d grid_y: %d -- alpha: %d color: %s'
-#                    print fmt % (x,y,grid_x,grid_y, opts['alpha'], opts['color'])
 
-                    if color == 'black':
-                        buf2[b] = chr(int(0.0 * alpha + ord(buf[b]) * (1 - alpha)))
-                        buf2[g] = chr(int(0.0 * alpha + ord(buf[g]) * (1 - alpha)))
-                        buf2[r] = chr(int(0.0 * alpha + ord(buf[r]) * (1 - alpha)))
-                        buf2[a] = chr(255 - opts['alpha'])
-                    else:
-                        buf2[b] = chr(int(255.0 * alpha + ord(buf[b]) * (1 - alpha)))
-                        buf2[g] = chr(int(255.0 * alpha + ord(buf[g]) * (1 - alpha)))
-                        buf2[r] = chr(int(255.0 * alpha + ord(buf[r]) * (1 - alpha)))
-                        buf2[a] = chr(255 - opts['alpha'])
+    # This is based upon the "Introduction to compositing" section (section 5) 
+    # of W3C's "Compositing and Blending Level 1" documentation -- 
+    # http://dev.w3.org/fxtf/compositing-1/
 
-#                    buf2[b] = buf[b]
-#                    buf2[g] = buf[g]
-#                    buf2[r] = buf[r]
-#                    buf2[a] = chr(alpha_weights[grid_x][grid_y])
-#                    buf2[a] = chr(100)
+    def alpha_composite(self, pixel, opacity, color):
+        "Implementation of Porter-Duff compositing"
 
-        img2.put_back_raw_data(buf2)
-        self.save_image(img, img2)
+        # gather the normalized rgba components for the "background" layer --
+        # the original image pixel
+        norm_bg_pixel = [ i/255.0 for i in pixel ]
+        norm_bg_alpha = norm_bg_pixel.pop()
 
-    def save_image(self, image, newimg):
-        file = os.path.splitext((os.path.basename(image.filename)))[0] + '-grid.png'
-        dir  = os.path.dirname(image.filename)
+        # gather the normalized rgba components for the "foreground" layer --
+        # the "black" or "white" pixel layer with a random alpha opacity level
+        # based on the assigned grid section
+        norm_fg_pixel = [1,1,1] if color == 'white' else [0,0,0]
+        norm_fg_alpha = opacity[color]/255.0
+
+        # Step 1: Calculate the pre_composite RGB values
+        pre_composite_rgb = [None, None, None]
+        alpha_src = 1.0 - norm_fg_alpha
+
+        (r, g, b) = range(0,3)
+        for c in (r, g, b):
+            pre_composite_rgb[c] = \
+                (norm_fg_pixel[c] * norm_fg_alpha) + \
+                (norm_bg_pixel[c] * norm_bg_alpha * alpha_src)
+
+        # Step 2: Calculate the new composite "alpha" channel value
+        composite_alpha = norm_fg_alpha + norm_bg_alpha * alpha_src
+
+        # Step 3: Finalize to the correct composite color value
+        composite_color = [ c/composite_alpha for c in pre_composite_rgb ]
+        composite_color.append(composite_alpha)
+
+        # Step 4: Reset the RGBA values back to their original 0 to 255 integer
+        #         range values
+        final_rgba = tuple([ int(c * 255.0) for c in composite_color ])
+
+        return final_rgba
+
+    def save_image(self, image, orig_filename):
+        file = os.path.splitext((os.path.basename(orig_filename)))[0] + '-grid.png'
+        dir  = os.path.dirname(orig_filename)
         if not dir:
             dir = '.'
 
-        print 'dir: %s' % dir
-        print 'file: %s' % file
-
         newfile = '/'.join([dir,file])
-        print 'Saving gridified image to: %s' % newfile
-        newimg.save(newfile)
-
-    def get_pixel_index(self, x, y, width, height, channels=4):
-        b = x*width*channels + y*channels
-        (g, r, a) = (b+1, b+2, b+3)
-        return (b, g, r, a)
+        image.save(newfile, 'PNG')
+        return newfile
 
     def generate_alpha_weights(self, img):
-        max_hbox = int( math.ceil(img.width  / (self.grid_width  + 0.0)) )
-        max_vbox = int( math.ceil(img.height / (self.grid_height + 0.0)) )
-        print "max_hbox: %d -- max_vbox: %d" % (max_hbox, max_vbox)
+        (width, height) = img.size
+        max_hbox = int( math.ceil(width  / (self.grid_width  + 0.0)) )
+        max_vbox = int( math.ceil(height / (self.grid_height + 0.0)) )
+        print "# of grid columns: %d" % (max_hbox)
+        print "# of grid rows   : %d" % (max_vbox)
 
         weights = [
-            [self.random_opacity() for i in range(max_hbox)]
-            for j in range(max_vbox)
+            [self.random_opacity() for i in range(max_vbox)]
+            for j in range(max_hbox)
         ]
 
         return weights
 
     def random_opacity(self):
-        random_opacity = random.randint(self.min_opacity, self.max_opacity)
-        color = ('black', 'white')[random.randint(0,1)]
-        return {'alpha': random_opacity, 'color': color}
+        opacity = {}
+        for color in ('black', 'white'):
+            random_opacity = random.randint(self.min_opacity, self.max_opacity)
+            opacity[color] = random_opacity
+
+        return opacity
 
 def setup_args(parser):
     parser.add_argument(
-        "-v", 
-        "--verbose", 
+        "-v",
+        "--verbose",
         help = "echo the string you use here",
         action = "store_true"
     )
@@ -168,4 +191,5 @@ if __name__ == '__main__':
         max_opacity = args.max_opacity
     )
 
-    pa.gridify(args.image)
+    grid_file = pa.gridify(args.image)
+    print 'Saved gridified image to: %s' % grid_file
