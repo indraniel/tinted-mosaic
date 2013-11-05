ea2c3e47e770ce0ab081b81c9bafac40f17fcac0
Indraniel <indraniel@gmail.com>
2013-11-04 22:33:59 -0600

    + formalize project and repo structure
      - This setup was based on:
    
        http://kennethreitz.org/repository-structure-and-python/

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='mosaic',
+    version='0.0.1',
+    description="Create images similar to Delta909's Pixel Matrix on arbitrary pictures",
+    long_description=readme,
+    author='Indraniel Das',
+    author_email='indraniel@gmail.com',
+    url='https://github.com/indraniel/tinted-mosaic',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
